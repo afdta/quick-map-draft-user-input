48d2837664557aa42789bac74e26aca8db3f68b4
Alec Friedhoff <afriedhoff@gmail.com>
2015-12-07 12:29:29 -0500

    Checked lat lon data -- ALL GOOD.

--- build/Gen lonlat.R
+++ build/Gen lonlat.R
@@ -7,6 +7,6 @@ all  <- metropops(FALSE,"2013")
 diff <- all[!(all$CBSA_Code %in% t100$CBSA_Code), ]
 lst <- list(t100 = t100[,c("CBSA_Code","CBSA_Title","POP2010","EST2013","lon","lat")], rest = diff[,c("CBSA_Code","CBSA_Title","POP2010","EST2013","lon","lat")])
 
-json <- list(lst)
+json <- toJSON(lst)
 
-writeLines(json, "/home/alec/Dropbox/Projects/Brookings/DataViz/QuickMap/LonLat.json")
+writeLines(json, "/home/alec/Projects/quick-map/build/LonLatUpdate.json")
